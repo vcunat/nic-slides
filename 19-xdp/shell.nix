da91404ccbc8254d3cdca2c99d6d2e01d5b33b42
Vladimír Čunát <vladimir.cunat@nic.cz>
2019-09-27 18:00:38 +0200

    19-xdp: start WIP, a few slides

---
+++ 19-xdp/shell.nix
@@ -0,0 +1,24 @@
+{ nixpkgs ? import <nixpkgs> { config = { allowUnfree = true; }; } }:
+with nixpkgs.pkgs;
+stdenv.mkDerivation {
+  name = "xetex";
+  src = ./.;
+  buildInputs = [
+    (texlive.combine {
+      inherit (texlive) scheme-small collection-xetex
+        fontspec euenc metapost metafont
+        pdfcrop
+        ;
+    })
+    gnumake
+  ];
+
+  #FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ lmodern corefonts ]; };
+
+  shellHook = ''
+    while true; do
+      yes X | make --silent
+      sleep 2
+    done
+  '';
+}
